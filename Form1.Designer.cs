28df04621bdf5bd6fc780ac0e51359fca80ca642
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-04-26 20:50:05 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace World_Series_Champions
+{
+    partial class frmWorldSeriesChampions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lboxTeams = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblOutNumWins = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a Team";
+            // 
+            // lboxTeams
+            // 
+            this.lboxTeams.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lboxTeams.FormattingEnabled = true;
+            this.lboxTeams.ItemHeight = 26;
+            this.lboxTeams.Location = new System.Drawing.Point(17, 38);
+            this.lboxTeams.Name = "lboxTeams";
+            this.lboxTeams.Size = new System.Drawing.Size(561, 212);
+            this.lboxTeams.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number of Times Won:";
+            // 
+            // lblOutNumWins
+            // 
+            this.lblOutNumWins.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutNumWins.Font = new System.Drawing.Font("Arsenal", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutNumWins.Location = new System.Drawing.Point(204, 270);
+            this.lblOutNumWins.Name = "lblOutNumWins";
+            this.lblOutNumWins.Size = new System.Drawing.Size(374, 40);
+            this.lblOutNumWins.TabIndex = 3;
+            this.lblOutNumWins.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(17, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Get Results";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(323, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 50);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(468, 326);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 50);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frmWorldSeriesChampions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 399);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblOutNumWins);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lboxTeams);
+            this.Controls.Add(this.label1);
+            this.Name = "frmWorldSeriesChampions";
+            this.Text = "World Series Champions";
+            this.Load += new System.EventHandler(this.frmWorldSeriesChampions_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lboxTeams;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblOutNumWins;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace World_Series_Champions
+{
+    public partial class frmWorldSeriesChampions : Form
+    {
+        private List<string> teamList = new List<string>();
+        public frmWorldSeriesChampions()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          int numWins = 0;
+
+            if(lboxTeams.Items.Count > 0)
+            {
+                if(lboxTeams.SelectedIndex != -1)
+                {                    
+                    numWins = GetNumWins(lboxTeams.SelectedItem.ToString());
+                }
+                    
+            }
+
+            lblOutNumWins.Text = "This team won " + numWins.ToString() + " time(s).";
+
+        }
+
+        private int GetNumWins(string teamName)
+        {
+            List<string> winningList = new List<string>();
+            try
+            {
+                StreamReader inputFile = File.OpenText("WorldSeriesWinners.txt");
+
+                List<string> seriesWinners = new List<string>();
+
+                while(!inputFile.EndOfStream)
+                {
+                    seriesWinners.Add(inputFile.ReadLine());
+                }
+
+                inputFile.Close();
+
+                foreach (string winners in seriesWinners)
+                {
+                    if (winners == teamName) // should be ==
+                    {
+                        winningList.Add(winners);
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return winningList.Count;
+
+        }
+
+        private void frmWorldSeriesChampions_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader inputFile = File.OpenText("Teams.txt");
+
+                while (!inputFile.EndOfStream)
+                {
+                    teamList.Add(inputFile.ReadLine());
+                }
+
+                inputFile.Close();
+
+                for (int i = 0; i < teamList.Count; i++)
+                {
+                    lboxTeams.Items.Add(teamList[i]);
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblOutNumWins.Text = "";
+            lboxTeams.SelectedItem = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
